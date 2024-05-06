72c392466e2ca8751108501e8f48112b370ffffe
mikeythebyers <169052405+mikeythebyers@users.noreply.github.com>
2024-05-06 17:36:33 +0530

    mike.sol

--- fx-portal-main/contracts/mike.sol
+++ fx-portal-main/contracts/mike.sol
@@ -4,18 +4,18 @@ pragma solidity ^0.8.9;
 
 import "erc721a/contracts/ERC721A.sol";
 
-contract nomme is ERC721A {
+contract mike is ERC721A {
     address public owner;
 
     uint256 public maxQuantity = 5;
 
     
     string baseUrl =
-        "https://indigo-immense-barnacle-223.mypinata.cloud/ipfs/QmYAguVHDGpTpHTP1bpG9VmVhZHJt5RHQii4TvAxbCWUqo/";
+        you url;
 
 
-    string public prompt = "not me";
-    constructor() ERC721A("CuteCats", "CC") {
+    string public prompt = "i'm mike byers";
+    constructor() ERC721A("mikebyers", "MB") {
         owner = msg.sender;
     }
 
